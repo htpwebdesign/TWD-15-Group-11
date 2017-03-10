0f1a2c59a08a587494fa5efc55875e4ecc255cd5
Reza Chalipour <mchpour@gmail.com>
2017-03-10 00:18:11 -0800

    footer fixed-order.btm done-some fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
-scripts.js
+//// JavaScript Document
+
+var $body = $('body');
+var $btnMobileMenu = $('.hamburger');
+
+
+$body.removeClass('show-mobile-menu');
+
+$btnMobileMenu.click(function(){
+	
+	
+    alert('hello');
+	$body.toggleClass('show-mobile-menu');
+	
+});
